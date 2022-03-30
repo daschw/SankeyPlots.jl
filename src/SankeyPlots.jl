fd9adb0e6f3021ec5715dc23b8a97f78702f2212
Benny <mail2benny+github@gmail.com>
2022-03-30 12:33:40 +0200

    Add option to control edge color manually

--- src/SankeyPlots.jl
+++ src/SankeyPlots.jl
@@ -20,7 +20,7 @@ In addition to [Plots.jl attributes](http://docs.juliaplots.org/latest/attribute
 |---|---|----|
 | `node_labels` | `nothing` | `AbstractVector{<:String}` |
 | `node_colors` | `nothing` | Vector of [color specifications supported by Plots.jl](http://docs.juliaplots.org/latest/colors/) or [color palette](http://docs.juliaplots.org/latest/generated/colorschemes/#ColorPalette) |
-| `edge_color` | `:gray` | Plots.jl supported [color](http://docs.juliaplots.org/latest/colors/) or color selection from connected nodes with `:src`, `:dst` or `:gradient` |
+| `edge_color` | `:gray` | Plots.jl supported [color](http://docs.juliaplots.org/latest/colors/) or color selection from connected nodes with `:src`, `:dst`, `:gradient` or an `AbstractDict{Tuple{Int64, Int64}, <:Colorant}` where `edge_color[(src, dst)]` maps to a color |
 | `label_position` | `:inside` | `:legend`, `:node`, `:left`, `:right`, `:top` or `:bottom` |
 | `label_size` | `8` | `Int` |
 | `compact` | `false` | `Bool` |
@@ -121,6 +121,8 @@ In addition to [Plots.jl attributes](http://docs.juliaplots.org/latest/attribute
                             fillcolor := node_colors[mod1(i, end)]
                         elseif edge_color === :dst
                             fillcolor := node_colors[mod1(k, end)]
+                        elseif typeof(edge_color) <: AbstractDict{Tuple{Int64, Int64}, <: Colorant}
+                            fillcolor := edge_color[(i, k)]
                         else
                             fillcolor := edge_color
                         end
